8728c85c20a2904ca05d63c816ac43507d4ca452
Abdi kumela <abdikumela@036gmail.com>
2024-02-19 18:12:07 -0800

    file read and write

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const greet = "Hello World";
-console.log(greet);
+const fileSys = require("fs");
+
+const readText = fileSys.readFileSync("./txt/file.txt", "utf-8");
+console.log(readText);
+
+const writeText = `This is a text written to a file for demonstration to a: ${readText}cl\n just an example to understand.`;
+fileSys.writeFileSync("./txt/file.txt", writeText);
+console.log("file written successfully.");
